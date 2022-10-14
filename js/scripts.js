d63171b8fbafb8f80e0e48aec2ef21ed4d716df1
joanafm81 <114578901+joanafm81@users.noreply.github.com>
2022-10-14 15:23:54 +0100

    Added new variable to script.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
-alert('Hello world');
-let favoriteFood = 'Lasagna';
-document.write(favoriteFood);
+let pokemonList = [];
+pokemonList = [
+  { name: 'Ivysaur', height: 1, type: ['Grass', 'Poison'] },
+  { name: 'Butterfree', height: 1.1, type: ['Bug', 'Flying'] },
+  { name: 'Noctowl', height: 1.6, type: ['Flying', 'Normal'] },
+  { name: 'Azumarill', height: 0.8, type: ['Fairy', 'Water'] },
+  { name: 'Cobalion', height: 2.1, type: ['Steel', 'Fighting'] },
+];
