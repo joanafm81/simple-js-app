7a0790fd1801d1a8f098c48bfabdab126e11e14e
joanafm81 <114578901+joanafm81@users.noreply.github.com>
2022-10-20 10:39:04 +0100

    Created Pokedex page
    Displayed list of Pokémons based on array; styled with Google fonts and basic rules.

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,13 @@ pokemonList = [
   { name: 'Azumarill', height: 0.8, type: ['Fairy', 'Water'] },
   { name: 'Cobalion', height: 2.1, type: ['Steel', 'Fighting'] },
 ];
+//For loop that iterates over each item in pokemonList
+for (let i = 0; i < pokemonList.length; i++) {
+	//Conditional to check if the Pokémon's height is above 2.0 and print "Wow that's big!" for the largest Pokémon
+	let highlight = '';
+	if (pokemonList[i].height > 2.0) {
+		highlight = " - Wow, that\’s big!";
+	}
+
+	document.write(`<li>${pokemonList[i].name} (height: ${pokemonList[i].height}) ${highlight}</li>`);
+}
