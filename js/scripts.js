199d67fc4d2c40119924fa3a5f20ada2d707e1f5
joanafm81 <114578901+joanafm81@users.noreply.github.com>
2022-10-26 14:10:27 +0100

    Replaced existing loop and added functions to manage Pokédex
    Added forEach() function to iterate over Pokémon; added functions to add a single Pokémon to an array, return all Pokémons and find specific Pokémon only by name.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,55 @@
-let pokemonList = [];
-pokemonList = [
-  { name: 'Ivysaur', height: 1, type: ['Grass', 'Poison'] },
-  { name: 'Butterfree', height: 1.1, type: ['Bug', 'Flying'] },
-  { name: 'Noctowl', height: 1.6, type: ['Flying', 'Normal'] },
-  { name: 'Azumarill', height: 0.8, type: ['Fairy', 'Water'] },
-  { name: 'Cobalion', height: 2.1, type: ['Steel', 'Fighting'] },
-];
-//For loop that iterates over each item in pokemonList
-for (let i = 0; i < pokemonList.length; i++) {
-	//Conditional to check if the Pokémon's height is above 2.0 and print "Wow that's big!" for the largest Pokémon
-	let highlight = '';
-	if (pokemonList[i].height > 2.0) {
-		highlight = " - Wow, that\’s big!";
+let pokemonRepository = (function () {
+	
+	let pokemonList = [];
+
+	function add (pokemon) {
+		if (typeof pokemon === 'object' && pokemon.name && pokemon.height && pokemon.type && Object.keys(pokemon).length === 3) {
+			pokemonList.push(pokemon);
+		} else {
+			return `${pokemon} is not a Pokémon. Pokémon must be an object with the keys name, height and type`;
+		}
 	}
 
-	document.write(`<li>${pokemonList[i].name} (height: ${pokemonList[i].height}) ${highlight}</li>`);
+	function getAll () {
+		return pokemonList
+	}
+
+	return {
+		add: add,
+		getAll: getAll
+
+	};
+
+}) ();
+
+pokemonRepository.add({ name: 'Ivysaur', height: 1, type: ['Grass', 'Poison'] });
+pokemonRepository.add({ name: 'Butterfree', height: 1.1, type: ['Bug', 'Flying'] });
+pokemonRepository.add({ name: 'Noctowl', height: 1.6, type: ['Flying', 'Normal'] });
+pokemonRepository.add({ name: 'Azumarill', height: 0.8, type: ['Fairy', 'Water'] });
+pokemonRepository.add({ name: 'Cobalion', height: 2.1, type: ['Steel', 'Fighting'] });
+
+let pokemonList = pokemonRepository.getAll();
+
+
+//forEach() function to iterate over the Pokémon in the pokemonList array (external function)
+pokemonList.forEach(printDetails);
+function printDetails (pokemon) {
+	let highlight = '';
+	if (pokemon.height > 2.0) {
+		highlight = " - Wow, that\’s big!";
+	}
+	document.write(`<li>${pokemon.name} (height: ${pokemon.height}) ${highlight}</li>`);
 }
+
+ let pokemonNames = [];
+ for (let i = 0; i < pokemonList.length; i++) {
+   pokemonNames.push(pokemonList[i].name);
+ }
+
+function findPokemons (query) {
+	return pokemonNames.filter(function (name) {
+		return name.toLowerCase().indexOf(query.toLowerCase()) > -1;
+	});
+}
+
+console.log(findPokemons('Iv'))
