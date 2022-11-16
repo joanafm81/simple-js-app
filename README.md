1e18828e61d2492c95c95fd059450167cd37b43e
joanafm81 <114578901+joanafm81@users.noreply.github.com>
2022-11-16 11:31:57 +0000

    Edited the ShowDetails function to show a modal with the Pokémon’s details. Made CSS changes.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Project description
+
+Small Pokedéx app (web application with HTML, CSS, and JavaScript) that loads data from an external source (API), displays a list of Pokemón and enables the user to view details for each Pokémon.
+
+# How to get the project running
+
+# Project dependencies
+
+* JavaScript version: ES6
+* ESLint rules:
+
+# Which API the project uses
+
+[PokÉAPI] (https://pokeapi.co/api/v2/pokemon/ditto)
+
--- js/scripts.js
+++ js/scripts.js
@@ -30,9 +30,64 @@ let pokemonRepository = (function () {
 
 	function showDetails(pokemon) {
 		pokemonRepository.loadDetails(pokemon).then(function() {
-		console.log(pokemon);
+			console.log(pokemon);
+			showModal(pokemon.name, pokemon.height, pokemon.imageUrl);
 		});
 	}
+
+	function showModal(name, height, imageUrl) {
+		let modalContainer = document.querySelector('#modal-container');
+		
+		//Clear all existing modal content
+		modalContainer.innerText = ' ';
+
+		let modal = document.createElement ('div');
+		modal.classList.add('modal');
+
+		//Add the new modal content
+		let closeButtonElement = document.createElement('button');
+		closeButtonElement.classList.add('modal-close');
+		closeButtonElement.innerText = 'Close';
+		closeButtonElement.addEventListener('click', hideModal);
+
+		let titleElement = document.createElement('h1');
+		titleElement.innerText = name;
+
+		let contentElement = document.createElement('p');
+		contentElement.innerText = 'Height: ' + height;
+
+		let imageElement = document.createElement('img');
+		imageElement.classList.add('image');
+		imageElement.src = imageUrl;
+
+
+		modal.appendChild(closeButtonElement);
+		modal.appendChild(titleElement);
+		modal.appendChild(contentElement);
+		modal.appendChild(imageElement)
+		modalContainer.appendChild(modal);
+		
+		modalContainer.classList.add('is-visible');
+
+		modalContainer.addEventListener('click', (e) => {
+			let target = e.target;
+			if (target === modalContainer) {
+				hideModal();
+			}
+		});
+	}
+
+	function hideModal() {
+		let modalContainer = document.querySelector('#modal-container');
+		modalContainer.classList.remove('is-visible');
+	}
+
+	window.addEventListener('keydown', (e) => {
+		let modalContainer = document.querySelector('#modal-container');
+		if(e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+			hideModal();
+		}
+	});
 
 	function loadList() {
 		showLoadingMessage();
